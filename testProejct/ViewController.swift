94fbda63d660486f28d041219e94ab3c4f7c40ee
Sandroshvili <63731060+Sandroshvili@users.noreply.github.com>
2020-07-03 14:18:41 +0400

    TestCommit

---
+++ testProejct/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testProejct
+//
+//  Created by Sandroshvili on 7/3/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
