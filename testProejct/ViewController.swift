1fc0aa5ef1a8231d80b5fd0fb1f053782004205b
Sandroshvili <63731060+Sandroshvili@users.noreply.github.com>
2020-07-03 14:22:01 +0400

    Second Test Commit

--- testProejct/ViewController.swift
+++ testProejct/ViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("I'm just here to test")
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
