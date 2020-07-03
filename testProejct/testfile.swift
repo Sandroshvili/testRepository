c9c1713e1f36e05cec47a51959a1cf805d699bc7
KatoMagradze <63748491+KatoMagradze@users.noreply.github.com>
2020-07-03 14:51:18 +0400

    added testfile

---
+++ testProejct/testfile.swift
@@ -0,0 +1,17 @@
+//
+//  testfile.swift
+//  testProejct
+//
+//  Created by Kato on 7/3/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import Foundation
+
+class testfile {
+    
+    func testfile() {
+        print("Test file")
+    }
+    
+}
