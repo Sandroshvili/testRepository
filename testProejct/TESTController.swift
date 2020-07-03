325647163cc07fc04828b8cd363c8aba63ac14a8
Sandroshvili <63731060+Sandroshvili@users.noreply.github.com>
2020-07-03 14:34:59 +0400

    Commit Remote

---
+++ testProejct/TESTController.swift
@@ -0,0 +1,33 @@
+//
+//  TESTController.swift
+//  testProejct
+//
+//  Created by Sandroshvili on 7/3/20.
+//  Copyright © 2020 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class TESTController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Here to test second ViewController, from search")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
